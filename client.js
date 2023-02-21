4fa6ba02f3068d9df97f772b01db41348c0e1b83
Ghasem Kiani <ghasemkiani@gmail.com>
2023-02-21 21:23:10 +0330

    update

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-import {Gitlab} from '@gitbeaker/node';
+import {Gitlab} from "@gitbeaker/node";
 
 import {cutil} from "@ghasemkiani/base";
 import {Obj} from "@ghasemkiani/base";
@@ -23,20 +23,33 @@ class Client extends Obj {
 	set api(api) {
 		this._api = api;
 	}
-	async toGetAllProjects() {
-		let app = this;
-		let {api} = app;
-		let projects = await api.Projects.all({
+	async toGetAllGroups() {
+		let client = this;
+		let {api} = client;
+		let groups = await api.Groups.all({
+			owned: true,
 			perPage: 100,
 			maxPages: 10,
 			showExpanded: false,
-			owned: true,
 		});
+		// path, id
+		return groups;
+	}
+	async toGetAllProjects() {
+		let client = this;
+		let {api} = client;
+		let projects = await api.Projects.all({
+			owned: true,
+			perPage: 100,
+			maxPages: 10,
+			showExpanded: false,
+		});
+		// path_with_namespace
 		return projects;
 	}
 	async toGet(path) {
-		let app = this;
-		let {url, token} = app;
+		let client = this;
+		let {url, token} = client;
 		let rsp = await fetch(url + path, {
 			method: "GET",
 			headers: {
@@ -47,8 +60,8 @@ class Client extends Obj {
 		return json;
 	}
 	async toPost(path, data) {
-		let app = this;
-		let {url, token} = app;
+		let client = this;
+		let {url, token} = client;
 		let rsp = await fetch(url + path, {
 			method: "POST",
 			headers: {
@@ -61,8 +74,8 @@ class Client extends Obj {
 		return json;
 	}
 	async toPut(path, data) {
-		let app = this;
-		let {url, token} = app;
+		let client = this;
+		let {url, token} = client;
 		let rsp = await fetch(url + path, {
 			method: "PUT",
 			headers: {
@@ -75,26 +88,35 @@ class Client extends Obj {
 		return json;
 	}
 	async toDelete(path) {
-		let app = this;
-		let {url, token} = app;
+		let client = this;
+		let {url, token} = client;
 		let rsp = await fetch(url + path, {
 			method: "DELETE",
 			headers: {
 				"PRIVATE-TOKEN": token,
 			},
 		});
-		let json = await rsp.json();
+		let json = await rsp.text();
+		return json;
+	}
+	async toGetProject({id}) {
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
+		let path = `/projects/${id}`;
+		let json = await client.toGet(path);
 		return json;
 	}
 	async toListRemoteMirrors({id}) {
-		let app = this;
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
 		let path = `/projects/${id}/remote_mirrors`;
-		let json = await app.toGet(path);
+		let json = await client.toGet(path);
 		return json;
 	}
 	async toCreatePushMirror({id, url, enabled, keep_divergent_refs, only_protected_branches}) {
-		let app = this;
-		let path = `/projects/${id}/remote_mirrors/${mirror_id}`;
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
+		let path = `/projects/${id}/remote_mirrors`;
 		if (cutil.isNil(enabled)) {
 			enabled = true;
 		}
@@ -110,33 +132,97 @@ class Client extends Obj {
 			keep_divergent_refs,
 			only_protected_branches,
 		};
-		let json = await app.toPost(path, data);
+		let json = await client.toPost(path, data);
 		return json;
 	}
 	async toGetRemoteMirror({id, mirror_id}) {
-		let app = this;
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
 		let path = `/projects/${id}/remote_mirrors/${mirror_id}`;
-		let json = await app.toGet(path);
+		let json = await client.toGet(path);
 		return json;
 	}
 	async toUpdateRemoteMirror({id, mirror_id, enabled, keep_divergent_refs, only_protected_branches}) {
-		let app = this;
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
 		let path = `/projects/${id}/remote_mirrors/${mirror_id}`;
 		let data = Object.entries({enabled, keep_divergent_refs, only_protected_branches}).reduce((data, [k, v]) => (cutil.isNil(v) ? data : ((data[k] = v), data)), {});
-		let json = await app.toPut(path, data);
+		let json = await client.toPut(path, data);
 		return json;
 	}
 	async toDeleteRemoteMirror({id, mirror_id}) {
-		let app = this;
+		let client = this;
+		id = encodeURIComponent(id); // id or path_with_namespace
 		let path = `/projects/${id}/remote_mirrors/${mirror_id}`;
-		let json = await app.toDelete(path);
+		let json = await client.toDelete(path);
 		return json;
+	}
+	async toCreateRepo({name, desc, org, pub}) {
+		let client = this;
+		let {api} = client;
+		let arg = {
+			path: name,
+			initialize_with_readme: false,
+			"public": !!pub,
+		};
+		if (desc) {
+			arg.description = desc;
+		}
+		if (org) {
+			let groups = await client.toGetAllGroups();
+			// namespace_id
+			org = groups.find(({path, id}) => (path === org)).id;
+		}
+		let result = await api.Projects.create(arg);
+		return result;
+	}
+	async toCreateRepoByUri(uri, pub) {
+		let client = this;
+		let username = await client.toGetUsername();
+		console.log(`Gitlab username: [${username}]`);
+		let [org, name] = uri.split("/");
+		console.log(`org: [${org}]`);
+		if (!name) {
+			name = org;
+			org = username;
+		}
+		if (org == username) {
+			org = null;
+		}
+		let desc = null;
+		let result = await client.toCreateRepo({name, desc, org, pub});
+		return result;
+	}
+	async toCreatePublicRepo(uri) {
+		let client = this;
+		let pub = true;
+		let result = await client.toCreateRepoByUri(uri, pub);
+		return result;
+	}
+	async toCreatePrivateRepo(uri) {
+		let client = this;
+		let pub = false;
+		let result = await client.toCreateRepoByUri(uri, pub);
+		return result;
+	}
+	async toGetUser() {
+		let client = this;
+		let user = await client.toGet("/user");
+		// username
+		return user;
+	}
+	async toGetUsername() {
+		let client = this;
+		let {username} = await client.toGetUser();
+		return username;
 	}
 }
 cutil.extend(Client.prototype, {
 	_token: null,
 	_api: null,
 	url: "https://gitlab.com/api/v4",
-})
+});
 
 export {Client};
+
+// https://docs.gitlab.com/ee/api/projects.html#create-project
